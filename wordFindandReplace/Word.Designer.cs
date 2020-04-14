992c94e010c82128385dda22078fc9e28cae89cb
Atakan Çelik <atakan@atakancelik.com.tr>
2020-04-15 00:42:01 +0300

    word find and replace

---
+++ wordFindandReplace/Word.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MertTestUygulama
+{
+    partial class Word
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lst_docs = new System.Windows.Forms.ListBox();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.btn_savepath = new System.Windows.Forms.Button();
+            this.btn_write = new System.Windows.Forms.Button();
+            this.check_accept = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lst_docs
+            // 
+            this.lst_docs.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lst_docs.FormattingEnabled = true;
+            this.lst_docs.Location = new System.Drawing.Point(12, 67);
+            this.lst_docs.Name = "lst_docs";
+            this.lst_docs.Size = new System.Drawing.Size(85, 329);
+            this.lst_docs.TabIndex = 0;
+            this.lst_docs.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(12, 12);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(364, 20);
+            this.txt_path.TabIndex = 1;
+            // 
+            // btn_savepath
+            // 
+            this.btn_savepath.Location = new System.Drawing.Point(12, 38);
+            this.btn_savepath.Name = "btn_savepath";
+            this.btn_savepath.Size = new System.Drawing.Size(122, 23);
+            this.btn_savepath.TabIndex = 2;
+            this.btn_savepath.Text = "Kayıt yeri belirle";
+            this.btn_savepath.UseVisualStyleBackColor = true;
+            this.btn_savepath.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_write
+            // 
+            this.btn_write.Enabled = false;
+            this.btn_write.Location = new System.Drawing.Point(12, 441);
+            this.btn_write.Name = "btn_write";
+            this.btn_write.Size = new System.Drawing.Size(274, 23);
+            this.btn_write.TabIndex = 3;
+            this.btn_write.Text = "Eminim yeni dosyayı yaz.";
+            this.btn_write.UseVisualStyleBackColor = true;
+            this.btn_write.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // check_accept
+            // 
+            this.check_accept.AutoSize = true;
+            this.check_accept.Location = new System.Drawing.Point(12, 418);
+            this.check_accept.Name = "check_accept";
+            this.check_accept.Size = new System.Drawing.Size(274, 17);
+            this.check_accept.TabIndex = 4;
+            this.check_accept.Text = "Tüm bilgileri doğru ve eksiksiz doldurduğuma eminim.";
+            this.check_accept.UseVisualStyleBackColor = true;
+            this.check_accept.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Word
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 606);
+            this.Controls.Add(this.check_accept);
+            this.Controls.Add(this.btn_write);
+            this.Controls.Add(this.btn_savepath);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.lst_docs);
+            this.Name = "Word";
+            this.Text = "Form4";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form4_FormClosed);
+            this.Load += new System.EventHandler(this.Word_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lst_docs;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Button btn_savepath;
+        private System.Windows.Forms.Button btn_write;
+        private System.Windows.Forms.CheckBox check_accept;
+    }
+}
---
+++ wordFindandReplace/Word.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Doc;
+
+namespace MertTestUygulama
+{
+    public partial class Word : Form
+    {
+        private FileInfo[] Files;
+        List<TextBox> textboxs = new List<TextBox>();
+        Document document = new Document();
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        Dictionary<string, string> filesDictionary = new Dictionary<string, string>();
+        Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application { Visible = false };
+        private static Microsoft.Office.Interop.Word.Document aDoc;
+        private string selectedFilePath = "";
+        private string selectedSavePath = "";
+        public Word()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(lst_docs.SelectedItem.ToString());
+            selectedFilePath = filesDictionary[lst_docs.SelectedItem.ToString()];
+            createDirectory();
+        }
+        private void getDocxDocuments()
+        {
+            /*Set default save folder*/
+            selectedSavePath = Directory.GetCurrentDirectory();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            try
+            {
+                Files = directoryInfo.GetFiles("*.docx");
+                if (Files.Length >= 1)
+                {
+                    foreach (FileInfo file in Files)
+                    {
+                        lst_docs.Items.Add(file.Name);
+                        filesDictionary.Add(file.Name,file.FullName);
+                    }
+                }
+                else
+                {
+                    lst_docs.Items.Add("Not found.");
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("There was a problem getting the files: " + exception);
+            }
+        }
+        private void createDirectory()
+        {
+
+            Regex regex = new Regex("<[^>]*>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            document.LoadFromFile(selectedFilePath);
+            MatchCollection matches = regex.Matches(document.GetText());
+            foreach (var match in matches)
+            {
+                if (!dictionary.ContainsKey(match.ToString()))
+                    dictionary.Add(match.ToString(), "Adjust the allowance.");
+            }
+            int xindextext = 280;
+            int xindexlabel = 185;
+            int y = 0;
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+                if (i%10 == 0)
+                {
+                    xindexlabel += 97;
+                    xindextext += 97;
+                    y = 0;
+                }
+
+                TextBox newTextBox = new TextBox();
+                newTextBox.Location = new Point(xindextext, 22 + (25 * y));
+                newTextBox.Size = new Size(240, 30);
+                newTextBox.Text = dictionary.ElementAt(i).Value;
+                this.Controls.Add(newTextBox);
+                textboxs.Add(newTextBox);
+
+                List<Label> labels = new List<Label>();
+                Label newLabel = new Label();
+                newLabel.Location = new Point(xindexlabel, 22 + (25 * y));
+                newLabel.Text = dictionary.ElementAt(i).Key;
+                this.Controls.Add(newLabel);
+                labels.Add(newLabel);
+                y++;
+            }
+        }
+
+        private void replaceandSave()
+        {
+            string filePath = selectedFilePath;
+            string fileName = filesDictionary.FirstOrDefault(x => x.Value == selectedFilePath).Key;
+            aDoc = wordApp.Documents.OpenNoRepairDialog(filePath);
+
+            foreach (var theVariable in dictionary)
+            {
+                FindAndReplace(wordApp, theVariable.Key, theVariable.Value);
+            }
+            aDoc.SaveAs(selectedSavePath+"\\"+"new_"+fileName);
+            aDoc.Close(true);
+        }
+        private void FindAndReplace(Microsoft.Office.Interop.Word.Application doc, object findText, object replaceWithText)
+        {
+            //options
+            object matchCase = false;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+            //execute find and replace
+            doc.Selection.Find.Execute(ref findText, ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundsLike, ref matchAllWordForms, ref forward, ref wrap, ref format, ref replaceWithText, ref replace,
+                ref matchKashida, ref matchDiacritics, ref matchAlefHamza, ref matchControl);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+            selectedSavePath = folderBrowserDialog.SelectedPath;
+            txt_path.Text = selectedSavePath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (check_accept.Checked)
+            {
+                for (int i = 0; i < textboxs.Count; i++)
+                {
+                    TextBox text = textboxs[i];
+                    dictionary[dictionary.ElementAt(i).Key] = text.Text;
+                }
+
+                replaceandSave();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (check_accept.Checked)
+                btn_write.Enabled = true;
+            else
+                btn_write.Enabled = false;
+        }
+
+        private void Form4_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private void Word_Load(object sender, EventArgs e)
+        {
+            getDocxDocuments();
+            txt_path.Text = selectedSavePath;
+        }
+    }
+}
